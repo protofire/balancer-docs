05d1532ef5983cd68f96844b039cc0cfad9a75a2
esis8 <eesis.dev@gmail.com>
2023-12-27 01:37:05 -0300

    Styles buttons

--- docs/.vuepress/components/ve8020/admin/AdminPools.vue
+++ docs/.vuepress/components/ve8020/admin/AdminPools.vue
@@ -38,10 +38,43 @@ const showRewards = async (id: string) => {
             veSystem.rewardDistributor.rewardNames || []
           "
         />
-        <button @click="showConfig(veSystem.id)">Config</button>
-        |
-        <button @click="showRewards(veSystem.id)">Rewards Distribution</button>
+        <div class="group-buttons">
+          <button class="btn-rewards" @click="showRewards(veSystem.id)">
+            Rewards Distribution
+          </button>
+          <button class="btn-config" @click="showConfig(veSystem.id)">
+            Pool configuration
+          </button>
+        </div>
       </div>
     </div>
   </section>
 </template>
+
+<style scoped>
+.group-buttons {
+  display: flex;
+  gap: 10px;
+  justify-content: flex-end;
+  max-width: 560px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  width: 90%;
+  margin-inline: auto;
+}
+.group-buttons .btn-rewards,
+.group-buttons .btn-config {
+  width: 150px;
+  height: 40px;
+  border-radius: 6px;
+  border: none;
+  background-color: #eaf0f6;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.dark .group-buttons .btn-rewards,
+.dark .group-buttons .btn-config {
+  background-color: #384aff;
+}
+</style>
