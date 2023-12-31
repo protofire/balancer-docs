a5f4755a726e06e69fefca1e2d4e981e91899340
esis8 <eesis.dev@gmail.com>
2023-12-31 15:38:24 -0300

    style early pelanty modal

--- docs/.vuepress/components/ve8020/admin/EarlyPenaltyModal.vue
+++ docs/.vuepress/components/ve8020/admin/EarlyPenaltyModal.vue
@@ -23,19 +23,24 @@ watch(
 
 <template>
   <div v-if="props.open" class="modal-container">
-    <div class="modal-popup early-unlock">
+    <div class="modal-popup early-penalty">
       <h3 class="modal-title">Set early penalty speed</h3>
       <div class="body">
         <div class="item-row">
           <p class="item-name">Early Penalty</p>
           <div class="input-group">
-            <input v-model="earlyPenaltyInput" type="number" class="input" />
+            <input
+              v-model="earlyPenaltyInput"
+              type="number"
+              class="input"
+              placeholder="10"
+            />
           </div>
         </div>
         <p class="text">
           The penalty coefficient indicating the penalty speed for early unlock.
           Must be between 0 and 50. Default value is 10 means that penalty has
-          linear speed
+          linear speed.
         </p>
       </div>
       <div class="btn-group">
@@ -52,6 +57,16 @@ watch(
 </template>
 
 <style scoped>
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type='number'] {
+  -moz-appearance: textfield;
+  appearance: textfield;
+}
 .modal-container {
   position: fixed;
   height: 100%;
@@ -62,6 +77,7 @@ watch(
   backdrop-filter: blur(1px);
   background-color: rgba(0, 0, 0, 0.2);
 }
+
 .modal-popup {
   position: sticky;
   border-radius: 16px;
@@ -70,8 +86,8 @@ watch(
   border: 1px solid #384aff;
   box-shadow: 0px 4px 20px 0px rgba(51, 65, 85, 0.5);
   width: 90%;
-  max-width: 320px;
-  left: calc(50% - 160px);
+  max-width: 560px;
+  left: calc(50% - 280px);
   padding: 16px 24px;
 }
 
@@ -83,7 +99,7 @@ watch(
 
 @media (max-width: 720px) {
   .modal-popup {
-    left: calc(50% - 160px);
+    left: calc(50% - 280px);
   }
 }
 
@@ -97,8 +113,8 @@ watch(
   background-color: #0d1834;
 }
 
-.modal-popup.early-unlock {
-  top: 380px;
+.modal-popup.early-penalty {
+  top: 350px;
   display: flex;
   flex-direction: column;
 }
@@ -111,11 +127,44 @@ watch(
 .modal-popup .body {
   margin-block: 20px 32px;
 }
-
 .modal-popup .body .text {
   margin: 0;
   font-size: 14px;
   font-weight: 500;
+}
+
+.modal-popup .body .item-row {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  height: 40px;
+  margin-bottom: 16px;
+  gap: 10px;
+}
+
+.item-row .item-name {
+  margin: 0;
+}
+
+.item-row .input-group {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  min-width: 270px;
+  justify-content: flex-end;
+}
+
+.item-row .input-group .input {
+  border-radius: 6px;
+  height: 100%;
+  width: 100%;
+  max-width: 340px;
+  padding-inline: 20px;
+  font-size: 14px;
+  outline: none;
+  border: 1px solid #384aff;
+  background-color: rgba(56, 74, 255, 0.2);
 }
 
 .modal-popup .btn-group {
