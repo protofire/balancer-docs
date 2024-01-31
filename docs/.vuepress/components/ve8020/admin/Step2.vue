2f7728cd91c38220ea8689dc98535f8910d6eb62
Luigi <luigibyte@gmail.com>
2024-01-31 13:34:05 -0300

    wip

---
+++ docs/.vuepress/components/ve8020/admin/Step2.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { createProviderComponent } from '../../../providers/createProviderComponent';
+import { provideNetwork } from '../../../providers/network';
+import { provideVeSystem } from '../../../providers/veSystem';
+
+import Wallet from '../../Navbar/Wallet.vue';
+import Launchpad from '../Launchpad.vue';
+
+const NetworkProvider = createProviderComponent(() => provideNetwork());
+const VeSystemProvider = createProviderComponent(() => provideVeSystem());
+</script>
+
+<template>
+  <NetworkProvider>
+    <VeSystemProvider>
+      <div class="network-select">
+        <Wallet />
+      </div>
+      <div class="main-container">
+        <div class="body-container">
+          <Launchpad />
+        </div>
+      </div>
+    </VeSystemProvider>
+  </NetworkProvider>
+</template>
+
+<style scoped>
+.network-select {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.main-container {
+  width: 100%;
+  margin-top: 16px;
+  border: 1px solid #e2e8f0;
+  border-radius: 6px;
+}
+
+.dark .main-container {
+  border: 1px solid #3e4c5a;
+}
+
+.body-container {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+</style>
