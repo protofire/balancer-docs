0a63cf1111e7f04731d9e52197d391c89e062cc4
Luigi <luigibyte@gmail.com>
2024-02-14 14:56:06 -0300

    update contract old addresses

--- docs/.vuepress/constants/config.ts
+++ docs/.vuepress/constants/config.ts
@@ -21,7 +21,7 @@ export const CONFIG = new Map<number, Config>([
     {
       SUBGRAPH_URL:
         'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_arb',
-      LAUNCHPAD_CONTRACT: '0x562cb67c3259120273bb7b75b9414638ab453af6',
+      LAUNCHPAD_CONTRACT: '0x665a23707e9cfce7bf07c52d375f5274cedd6eb4',
       LENS_REWARD_CONTRACT: '0xb422e74045f19d94ec62236efdcae4b13bac386b',
     },
   ],
@@ -30,7 +30,7 @@ export const CONFIG = new Map<number, Config>([
     {
       SUBGRAPH_URL:
         'https://subgraph.satsuma-prod.com/23d030b3f9c5/maksims-team--1931238/launchpad_polygon_zk/api',
-      LAUNCHPAD_CONTRACT: '0x6c9c2881b6e2fdb4e9e72896a4883049ad155b36',
+      LAUNCHPAD_CONTRACT: '0x665a23707e9cfce7bf07c52d375f5274cedd6eb4',
       LENS_REWARD_CONTRACT: '0xb422e74045f19d94ec62236efdcae4b13bac386b',
     },
   ],
@@ -39,8 +39,8 @@ export const CONFIG = new Map<number, Config>([
     {
       SUBGRAPH_URL:
         'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_polygon',
-      LAUNCHPAD_CONTRACT: '0x93a6a2c89022e60a94986621ca8a3cd45e31dc77',
-      LENS_REWARD_CONTRACT: '0xb422e74045f19d94ec62236efdcae4b13bac386b',
+      LAUNCHPAD_CONTRACT: '0x41b5b45f849a39cf7ac4aceae6c78a72e3852133',
+      LENS_REWARD_CONTRACT: '0xe922ae35d0cd042db5bade95e50a5cb0f14fa1d9',
     },
   ],
   [
@@ -62,7 +62,7 @@ export const CONFIG = new Map<number, Config>([
     },
   ],
   [
-    43114,
+    43114, // avalanche
     {
       SUBGRAPH_URL:
         'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_avax',
